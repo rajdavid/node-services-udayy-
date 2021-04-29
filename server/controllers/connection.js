af055ed8d967485b8bfecf6d649e392c18d11461
rajdavid <david.raj@techchefz.com>
2021-04-30 02:18:13 +0530

    posgress setup

---
+++ server/controllers/connection.js
@@ -0,0 +1,19 @@
+
+import  PostGresConnection from '../helpers/sequalize'
+
+
+async function getConnection(req, res) {
+  console.log('Post Gress Conncetion  ');
+ 
+  try {
+    await PostGresConnection.authenticate();
+    console.log('Connection has been established successfully.');
+    res.send({'connection':'successful'})
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    res.send({'connection':'failed'})
+  }
+}
+export default {
+  getConnection,
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
+
 import express from 'express';
 
 import healthCtrl from '../controllers/health';
-
+import { getConnection} from '../controllers/connection'
 const router = express.Router();
 // added some comment
 // mount auth routes at /auth
 router.get('/health-check', healthCtrl.checkConnection);
-
+router.get('/connetion', getConnection);
 export default router;
